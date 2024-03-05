6d12e067b760365afd341319d8c127ee1cbb95f3
Albert Meng <meng0052@algonquinlive.com>
2024-03-05 15:36:36 -0500

    cursor03

---
+++ MousmiS_AlbertM/cursor03.py
@@ -0,0 +1,28 @@
+import arcpy
+from arcpy import env
+import sys
+
+if len(sys.argv) != 2:
+        print("Usage: python script_name.py <province_abbreviation>")
+        sys.exit()
+
+province = sys.argv[1].upper()
+
+valid_provinces = ["AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"]
+if province not in valid_provinces:
+        print(f"Invalid province abbreviation: {province}")
+        sys.exit()
+
+shapefile_path = r"../../../data/Canada/Canada.gdb/MajorCities"
+env.workspace=shapefile_path
+    # Move the SearchCursor inside the main function
+with arcpy.da.SearchCursor(shapefile_path, ["Name", "Prov"], where_clause=f"{arcpy.AddFieldDelimiters(shapefile_path, 'Prov')} = '{province}'") as cursor:
+        cities = []
+        city_count = 0
+
+        for row in cursor:
+            city_name, province = row
+            city_count += 1
+            print(f"{city_name}, {province}")
+
+print(f"\nThere are {city_count} cities in {province}.")
